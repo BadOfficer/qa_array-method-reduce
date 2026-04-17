5bb1d6e9c313a510ca70771787ac38e34a01aa2b
BadOfficer <tarasbondarenko2005@gmail.com>
2026-04-17 16:17:09 +0300

    Solution

--- src/reduce.test.js
+++ src/reduce.test.js
@@ -3,15 +3,78 @@
 const { reduce } = require('./reduce');
 
 describe('reduce', () => {
+  let callback;
+
   beforeAll(() => {
     Array.prototype.reduce2 = reduce; // eslint-disable-line
+  });
+
+  beforeEach(() => {
+    callback = jest.fn();
   });
 
   afterAll(() => {
     delete Array.prototype.reduce2;
   });
 
-  it('should ', () => {});
+  it('should call cb for each value', () => {
+    const testArr = [1, 2, 3];
 
-  // Add tests here
+    testArr.reduce2(callback, 0);
+
+    expect(callback).toHaveBeenCalledTimes(3);
+  });
+
+  it('should pass to cb prevValue, value, index and array', () => {
+    const testArr = [1, 2, 3];
+
+    testArr.reduce2(callback, 0);
+
+    expect(callback).toHaveBeenNthCalledWith(1, 0, 1, 0, [1, 2, 3]);
+  });
+
+  it('should work without startValue', () => {
+    const testArr = [1, 2, 3];
+
+    testArr.reduce2(callback);
+
+    expect(callback).toHaveBeenNthCalledWith(1, 1, 2, 1, [1, 2, 3]);
+  });
+
+  it('should work with empty array with start value', () => {
+    const testArr = [];
+
+    const result = testArr.reduce2(callback, 1);
+
+    expect(callback).toHaveBeenCalledTimes(0);
+    expect(result).toEqual(1);
+  });
+
+  it('should work with empty array without start value', () => {
+    const testArr = [];
+
+    const result = testArr.reduce2(callback);
+
+    expect(callback).toHaveBeenCalledTimes(0);
+    expect(result).toBeUndefined();
+  });
+
+  it('should work with one item', () => {
+    const testArr = [1];
+    callback.mockReturnValue(2);
+
+    const result = testArr.reduce2(callback, 1);
+
+    expect(callback).toHaveBeenCalledTimes(1);
+    expect(result).toEqual(2);
+  });
+
+  it('should work with one item without start value', () => {
+    const testArr = [1];
+
+    const result = testArr.reduce2(callback);
+
+    expect(callback).toHaveBeenCalledTimes(0);
+    expect(result).toEqual(1);
+  });
 });
